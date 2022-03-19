7972f8b82930f14f8f1af4c49a967a23c6693698
Povilas Saveika <povilassaveika@gmail.com>
2022-03-19 16:18:18 +0200

    added landing page, main css and js

---
+++ js/navbar.js
@@ -0,0 +1,22 @@
+﻿const app = (() => {
+    let body;
+    let menu;
+    let menuItems;
+
+    const init = () => {
+        body = document.querySelector('body');
+        menu = document.querySelector('.navbar-icon');
+        menuItems = document.querySelectorAll('.navbar-list-item');
+        applyListeners();
+    };
+
+    const applyListeners = () => {
+        menu.addEventListener('click', () => toggleClass(body, 'navbar-active'));
+    };
+
+    const toggleClass = (element, stringClass) => {
+        if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+    };
+
+    init();
+})();
